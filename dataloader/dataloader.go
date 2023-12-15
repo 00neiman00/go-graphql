79e20d8f854abcb25f176219e846884ea6e1192d
00neiman00 <jongyun@seoulrobotics.org>
2023-12-15 16:31:55 +0900

    feat: fixing the dataloader issue

--- dataloader/dataloader.go
+++ dataloader/dataloader.go
@@ -2,10 +2,11 @@ package dataloader
 
 import (
 	"context"
-	"github.com/go-pg/pg/v10"
-	"github.com/neimen-95/go-graphql/models"
 	"net/http"
 	"time"
+
+	"github.com/go-pg/pg/v10"
+	"github.com/neimen-95/go-graphql/models"
 )
 
 const userLoaderKey = "userLoader"
@@ -15,8 +16,24 @@ func DataloaderMiddleWare(db *pg.DB, next http.Handler) http.Handler {
 		userLoader := UserLoader{
 			fetch: func(ids []string) ([]*models.User, []error) {
 				var users []*models.User
+
 				err := db.Model(&users).Where("id in (?)", pg.In(ids)).Select()
-				return users, []error{err}
+
+				if err != nil {
+					return nil, []error{err}
+				}
+				u := make(map[string]*models.User, len(users))
+
+				for _, user := range users {
+					u[user.ID] = user
+				}
+
+				result := make([]*models.User, len(ids))
+				for i, id := range ids {
+					result[i] = u[id]
+				}
+
+				return result, []error{err}
 			},
 			wait:     1 * time.Millisecond,
 			maxBatch: 100,
